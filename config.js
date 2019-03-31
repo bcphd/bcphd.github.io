8242a6bf3d574f7da87250fabf4b83191b943383
BC <bc@bcphd.com>
2019-03-31 10:34:58 -0400

    initial checkin

---
+++ config.js
@@ -0,0 +1,67 @@
+
+module.exports = {
+  pathPrefix: '/',
+  siteUrl: 'https://bcphd.github.io',
+  siteTitle: 'BC PhD',
+  siteDescription: 'coder & prof',
+  author: 'bc',
+  postsForArchivePage: 3,
+  defaultLanguage: 'en',
+  disqusScript: 'https://bcphd.disqus.com/embed.js',
+  pages: {
+    home: '/',
+    blog: 'blog',
+    about: 'about',
+    tag: 'tag',
+    archive: 'archive'
+  },
+  social: {
+    github: 'https://github.com/bcphd',
+    linkedin: 'https://www.linkedin.com/in/briancartercst/',
+    rss: '/rss.xml'
+  },
+  tags: {
+    angular: {
+      description: 'Angular is an open source web application platform.'
+    },
+    electron: {
+      description: 'Electron is a framework for building cross-platform desktop applications with web technology.'
+    },
+    javascript: {
+      description: 'JavaScript is an object-oriented programming language used alongside HTML and CSS to give functionality to web pages.'
+    },
+    laravel: {
+      description: 'Laravel is a PHP framework for building web applications following the MVC pattern.'
+    },
+    nodejs: {
+      name: 'Node.js',
+      description: 'Node.js is a tool for executing JavaScript in a variety of environments.'
+    },
+    rxjs: {
+      name: 'RxJS',
+      description: 'RxJS is a library for reactive programming using Observables, to make it easier to compose asynchronous or callback-based code.'
+    },
+    sass: {
+      description: 'Sass is a stable extension to classic CSS.'
+    },
+    typescript: {
+      description: 'TypeScript is a typed superset of JavaScript that compiles to plain JavaScript.'
+    },
+    react: {
+      description: 'React is an open source JavaScript library used for designing user interfaces.'
+    },
+    vuejs: {
+      name: 'Vue.js',
+      description: 'Vue.js is a JavaScript framework for building interactive web applications.'
+    },
+    datastructures: {
+      name: 'Data Structures',
+      description: 'Data structures is a course module on data structures and algorithms.'
+    },
+    dotnet: {
+      name: '.NET',
+      description: 'The .NET framework, is much more than just a software framework.'
+    }
+    
+  }
+}
